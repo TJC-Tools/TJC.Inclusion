4dff56d76db48b23220dfd676c8b50da7c29eee3
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-10 19:59:29 -0400

    Constructor Tests

---
+++ TJC.Inclusion.Tests/Constructor/InclusionConstructorTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TJC.Inclusion.Tests.Constructor;
+
+[TestClass]
+public class InclusionConstructorTests
+{
+    [TestMethod]
+    public void ConstructTrue_InternalTrue()
+    {
+        // Arrange
+        var inclusion = new Inclusion(true);
+
+        // Assert
+        Assert.IsTrue(inclusion._included);
+    }
+
+    [TestMethod]
+    public void ConstructFalse_InternalFalse()
+    {
+        // Arrange
+        var inclusion = new Inclusion(false);
+
+        // Assert
+        Assert.IsFalse(inclusion._included);
+    }
+}
