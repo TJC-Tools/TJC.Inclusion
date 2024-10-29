c78f0405a0280d9873c616fe14f964973b781801
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-28 21:43:51 -0400

    🧹 chore: [MegaLinter] Apply [1] automatic fixes (#59)
    * 🧹 chore: [MegaLinter] Apply [1] automatic fixes
    
    * Update .cspell.json
    
    ---------
    
    Co-authored-by: TylerCarrol <25536704+TylerCarrol@users.noreply.github.com>

--- TJC.Inclusion/Inclusion.cs
+++ TJC.Inclusion/Inclusion.cs
@@ -10,36 +10,27 @@ public class Inclusion(bool included = true)
 
     #region Methods
 
-    public void Include() =>
-        _included = true;
+    public void Include() => _included = true;
 
-    public void Exclude() =>
-        _included = false;
+    public void Exclude() => _included = false;
 
     #endregion
 
     #region Operators
 
-    public static implicit operator bool(Inclusion inclusion) =>
-        inclusion._included;
+    public static implicit operator bool(Inclusion inclusion) => inclusion._included;
 
-    public static implicit operator Inclusion(bool value) =>
-        new(value);
+    public static implicit operator Inclusion(bool value) => new(value);
 
-    public static bool operator true(Inclusion inclusion) =>
-        inclusion._included;
+    public static bool operator true(Inclusion inclusion) => inclusion._included;
 
-    public static bool operator false(Inclusion inclusion) =>
-        !inclusion._included;
+    public static bool operator false(Inclusion inclusion) => !inclusion._included;
 
-    public static bool operator !(Inclusion inclusion) =>
-        !inclusion._included;
+    public static bool operator !(Inclusion inclusion) => !inclusion._included;
 
-    public static Inclusion operator &(Inclusion x, Inclusion y) =>
-        new(x._included & y._included);
+    public static Inclusion operator &(Inclusion x, Inclusion y) => new(x._included & y._included);
 
-    public static Inclusion operator |(Inclusion x, Inclusion y) =>
-        new(x._included | y._included);
+    public static Inclusion operator |(Inclusion x, Inclusion y) => new(x._included | y._included);
 
     #endregion
 }
